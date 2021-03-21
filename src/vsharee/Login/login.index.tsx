4a166cfb0ea1ea3e5832398c14bef7eb246307d0
mobin <mobinsh_20@yahoo.com>
2021-03-21 20:55:14 +0330

    Fix: image

--- src/vsharee/Login/login.index.tsx
+++ src/vsharee/Login/login.index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './D:Vsharee\vSharee_frontsrcassetsimages\background.png';
 import { ReduxState } from 'interface';
 import { connect, ConnectedProps } from 'react-redux';
 import './login.style.scss';
@@ -7,6 +8,11 @@ const Login: React.FC<ConnectedProps<typeof connector>> = function (props: Conne
     return (
         <div className={'vsharee-login-page'}>
             <h1>Hi</h1>
+            <h1>HEloooooooooooooooooooo</h1>
+            <div className={'login-background'}>
+                console.log(logo);
+                <h1>HEloooooooooooooooooooo</h1>
+            </div>
         </div>
     );
 };
@@ -14,6 +20,9 @@ const Login: React.FC<ConnectedProps<typeof connector>> = function (props: Conne
 const mapStateToProps = (state: ReduxState) => ({
     text: state.language,
 });
+function Header() {
+    return <img src={logo} alt="Logo" />;
+}
 
 const connector = connect(mapStateToProps);
 export default connector(Login);
