8b3332aaa12b23f6f44100ffabbed34181253c39
mobin <mobinsh_20@yahoo.com>
2021-03-23 23:44:26 +0430

    Add: Text

--- src/vsharee/Login/login.index.tsx
+++ src/vsharee/Login/login.index.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import { ReduxState } from 'interface';
 import { connect, ConnectedProps } from 'react-redux';
 //import box from 'assets/images/box.png';
-//import RedBox from 'assets/images/RedBox.png';
+import RedBox from 'assets/images/RedBox.png';
 import './login.style.scss';
 
 const Login: React.FC<ConnectedProps<typeof connector>> = function (props: ConnectedProps<typeof connector>) {
     return (
         <div className={'vsharee-login-page'}>
             <div className={'Box'}>
-                <div className={'Redbox'}></div>
+                <div className={'Redbox'}>
+                    <img className={'Redboxbackground'} src={RedBox} />
+                    <h1 className={'Welcome-Back-'}>Welcome Back</h1>
+                    <h1 className={'Sign-in-to-continue'}>Sign in to continue access pages</h1>
+                </div>
 
                 <div className={'Blackbox'}></div>
             </div>
