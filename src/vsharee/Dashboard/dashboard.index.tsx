1413c02f0b7ad3a5f5facc9f38e655d81c3a73b3
mobin <mobinsh_20@yahoo.com>
2021-04-13 23:38:30 +0430

    Add: center div and online member

--- src/vsharee/Dashboard/dashboard.index.tsx
+++ src/vsharee/Dashboard/dashboard.index.tsx
@@ -23,18 +23,44 @@ const Dashboard: React.FC<ConnectedProps<typeof connector>> = function (props: C
 
     return (
         <div className="vsharee-dashboard-page">
-            <div className={"leftcolumn"}>
-                <div className={"top"}></div>
-                <div className={"bottom"}></div>
+            <div className="leftcolumn">
+                <div className={"top"}>
+                    <div className="top-in"></div>
+                </div>
+                <div className="bottom">
+                    <div className="bottom-in"></div>
+                </div>
             </div>
-            <div className={"centercolumn"}>
-                <img src={tvPic} alt="tvPic" />
+            <div className="centercolumn">
+                <div className="center-items">
+                   <img src={tvPic} alt="tvPic" />
+                   <h2>Welcome!</h2>
+                    <span>This is your brand, shiny server. Here are some steps </span>
+                    <h3>to help you et stared:</h3>
+                    <p>Create your group</p>
+                    <p>personalize your group with an icon</p>
+                    <p>Invite your friends</p>
+                    <p>Enjoy!</p>
+
+
+                </div>
+
             </div>
             <div className={"rightcolumn"}>
                 <div className={"rightcolumn-in"}>
                     <h3>My Friends</h3>
                     <div className={"context"}>
                         <div className="items">
+                            <div className={"left-items"}>
+                                <img src={fakePic} alt="fakePic"/>
+                                <i className="material-icons-outlined circle">circle</i>
+                            </div>
+                            <div className={"right-items"}>
+
+                            </div>
+
+                        </div>
+                        <div className="items online">
                             <div className={"left-items"}>
                                 <img src={fakePic} alt="fakePic"/>
                                 <i className="material-icons-outlined circle">circle</i>
