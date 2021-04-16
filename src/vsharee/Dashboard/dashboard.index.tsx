8b6bad44ef782b8de63481969d6131a3a49e1ba8
mobin <mobinsh_20@yahoo.com>
2021-04-16 23:45:46 +0430

    Add:Top Groups

--- src/vsharee/Dashboard/dashboard.index.tsx
+++ src/vsharee/Dashboard/dashboard.index.tsx
@@ -32,6 +32,7 @@ const Dashboard: React.FC<ConnectedProps<typeof connector>> = function (props: C
                             <div className="items-top">
 
                                 <div className="items-top-left">
+
                                     <img src={fakePic} alt="fakePic"/>
                                 </div>
                                 <div className="items-top-right">
@@ -65,6 +66,42 @@ const Dashboard: React.FC<ConnectedProps<typeof connector>> = function (props: C
 
 
                             </div>
+                            <div className="items-top">
+
+                                <div className="items-top-left">
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-top-right">
+                                    <p># Group Num4</p>
+                                </div>
+
+
+
+                            </div>
+                            <div className="items-top">
+
+                                <div className="items-top-left">
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-top-right">
+                                    <p># Group Num3</p>
+                                </div>
+
+
+
+                            </div>
+                            <div className="items-top">
+
+                                <div className="items-top-left">
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-top-right">
+                                    <p># Group Num6</p>
+                                </div>
+
+
+
+                            </div>
 
                         </div>
                     </div>
@@ -73,6 +110,92 @@ const Dashboard: React.FC<ConnectedProps<typeof connector>> = function (props: C
                 <div className="bottom">
                     <div className="bottom-in">
                         <h3>Top Groups</h3>
+                        <div className="my-container">
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                            <div className="items-bottom">
+                                <div className="items-bottom-left">
+
+                                    <i className="material-icons-outlined star">star</i>
+
+
+                                    <img src={fakePic} alt="fakePic"/>
+                                </div>
+                                <div className="items-bottom-right">
+                                    <p>Johnhayes</p>
+                                    <span>685 members</span>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -82,10 +205,14 @@ const Dashboard: React.FC<ConnectedProps<typeof connector>> = function (props: C
                    <h2>Welcome!</h2>
                     <span>This is your brand, shiny server. Here are some steps </span>
                     <h3>to help you et stared:</h3>
-                    <p>Create your group</p>
-                    <p>personalize your group with an icon</p>
-                    <p>Invite your friends</p>
-                    <p>Enjoy!</p>
+                    <div className="p-text">
+                        <p>● Create your group</p>
+                        <p>● personalize your group with an icon</p>
+                        <p>● Invite your friends</p>
+                        <p>● Enjoy!</p>
+
+
+                    </div>
 
 
                 </div>
